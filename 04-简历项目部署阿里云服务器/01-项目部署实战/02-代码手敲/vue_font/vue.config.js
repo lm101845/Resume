62062d2cbecd0accec3ead080a6ef4ef47f3e8e4
lm101845 <1018457683@qq.com>
2021-09-09 09:49:14 +0800

    Auto commit.

--- 04-简历项目部署阿里云服务器/01-项目部署实战/02-代码手敲/vue_font/vue.config.js
+++ 04-简历项目部署阿里云服务器/01-项目部署实战/02-代码手敲/vue_font/vue.config.js
@@ -4,6 +4,10 @@
  * @LastEditTime: 2021-09-09 08:41:16
  * @FilePath: \04-简历项目部署阿里云服务器\01-项目部署实战\02-代码手敲\vue_font\vue.config.js
  */
+
+//  这个主要为了解决Invalid Host header的问题
+// vue-cli配置域名访问报错：Invalid Host header（无效的主机头）
+// https://blog.csdn.net/qq_40678503/article/details/109206981
 module.exports = {
     productionSourceMap: false,
     devServer: {
--- 04-简历项目部署阿里云服务器/01-项目部署实战/02-代码手敲/vue_webhook/webhook.js
+++ 04-简历项目部署阿里云服务器/01-项目部署实战/02-代码手敲/vue_webhook/webhook.js
@@ -1,6 +1,21 @@
 /*
  * @Author: liming
  * @Date: 2021-09-09 06:54:10
- * @LastEditTime: 2021-09-09 06:55:05
- * @FilePath: \04-简历项目部署阿里云服务器\01-项目部署实战\02-代码手敲\vue_webhook\webhook.js
+ * @LastEditTime: 2021-09-09 09:46:32
+ * @FilePath: \01-项目部署实战\02-代码手敲\vue_webhook\webhook.js
  */
+let http = require('http')
+
+let server = http.createServer(function (req, res) {
+    if (req.method == 'POST' && req.url == '/webhook') {
+        res.setHeader('Content-Type', 'application/json')
+        // 我给GitHub服务器发送一个回应,告诉服务器我这边已经成功了
+        res.end(JSON.stringify({OK: true}))
+    } else {
+        res.end('Not Found')
+    }
+})
+
+server.listen(4000, () => {
+    console.log('webhook服务已经在4000端口上启动');
+})
